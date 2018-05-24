b04a3935c77b74c3088d6877501c0f62119fdc12
Sitao Liang <liangsitao.ach@gmail.com>
2018-05-24 22:50:58 +0800

    1. Scan the Post table to find the item with specific PostId.

--- GetPost/index.js
+++ GetPost/index.js
@@ -4,6 +4,9 @@ var AWS = require("aws-sdk");
 var docClient = new AWS.DynamoDB.DocumentClient();
 var table = 'Post';
 
+var scan_callback = null;
+var scan_event = null;
+
 exports.handler = (event, context, callback) => {
     var params = {
         TableName: table,
@@ -12,12 +15,33 @@ exports.handler = (event, context, callback) => {
         }
     };
 
-    docClient.get(params, function(err, data) {
-        if (err) {
-            callback(null, formatter.getResultError("Unable to read item. " + err));
-        } else {
-            callback(null, formatter.getResultSingle(data));
-            //callback(null, JSON.stringify(data, null, 2));
-        }
-    });
+    var params = {
+        TableName: table,
+        ProjectionExpression: "PostId, #ts, ImgUrl, LikeCount, Note, UserId",
+        FilterExpression: "PostId = :postid",
+        ExpressionAttributeNames: {
+            "#ts": "Timestamp",
+        },
+        ExpressionAttributeValues: {
+             ":postid": event.postid
+        },
+        ScanIndexForward: false
+    };
+
+    scan_callback = callback;
+    scan_event = event;
+    docClient.scan(params, onScan);
 };
+
+function onScan(err, data) {
+    if (err) {
+        scan_callback(null, formatter.getResultError("Unable to read item. " + err));
+    } else {
+        data.Items.forEach(function(post) {
+           scan_callback(null, formatter.getResultSingle(post));
+           return;
+        });
+
+        scan_callback(null, formatter.getResultError("Post is not found."));
+    }
+}
