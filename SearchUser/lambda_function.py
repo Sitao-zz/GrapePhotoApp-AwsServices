1c11415f1cd6d122cbc2f7a82ab7c670083343b3
Sitao Liang <liangsitao.ach@gmail.com>
2018-05-26 23:09:50 +0800

    1. Allow fuzzy query on the search value

--- SearchUser/lambda_function.py
+++ SearchUser/lambda_function.py
@@ -6,12 +6,12 @@ dynamodb_resource = resource('dynamodb')
 
 def lambda_handler(event, context):
     try:
+        if(len(event['username'])) <= 3:
+            response = getResultError("Search value must be more than 3 characters")
+            return response
+
         data = scan_table("User","UserName",event['username'])
-        result = {
-            "UserId": data['Items'][0]['UserId'],
-            "UserName": data['Items'][0]['UserName'],
-            "Email": data['Items'][0]['Email']
-        }
+        result = extractInfo(data['Items'])
         response = getResultMultiple(result)
     except Exception, e:
         response = getResultError(str(e))
@@ -20,8 +20,19 @@ def lambda_handler(event, context):
 def scan_table(table_name, filter_key=None, filter_value=None):
     table = dynamodb_resource.Table(table_name)
     if filter_key and filter_value:
-        filtering_exp = Attr(filter_key).eq(filter_value)
+        filtering_exp = Attr(filter_key).contains(filter_value)
         response = table.scan(FilterExpression=filtering_exp)
     else:
         response = table.scan()
     return response
+
+def extractInfo(users):
+    list = []
+    for user in users:
+        item = {
+            "UserId": user['UserId'],
+            "UserName": user['UserName'],
+            "Email": user['Email']
+        }
+        list.append(item)
+    return list
