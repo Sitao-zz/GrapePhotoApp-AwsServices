c44f2ccd4461703374b2f1a7308d4ef63a311f21
Sitao Liang <liangsitao.ach@gmail.com>
2018-05-27 11:14:25 +0800

    1. Only extract Followee for return

--- GetFollowees/lambda_function.py
+++ GetFollowees/lambda_function.py
@@ -11,11 +11,12 @@ dynamodb_resource = resource('dynamodb')
 
 def lambda_handler(event, context):
     filtering_exp = Attr('Follower').eq(event['userid'])
-    data = get_items("Follow",filtering_exp)
+    projection_exp='Followee'
+    data = get_items("Follow",filtering_exp,projection_exp)
     response=getResultMultiple(data['Items'])
     return response
 
-def get_items(table_name, expr):
+def get_items(table_name, filterExpr, projExpr):
     table = dynamodb_resource.Table(table_name)
-    data = table.scan(FilterExpression=expr)
+    data = table.scan(FilterExpression=filterExpr,ProjectionExpression=projExpr)
     return data
