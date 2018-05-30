abb8141ad829cfaea0978e7b21717dc257d9ff02
Sitao Liang <liangsitao.ach@gmail.com>
2018-05-31 00:52:38 +0800

    1. Return user information from /user/top

--- LambdaFunctions/GetTopUsers/lambda_function.py
+++ LambdaFunctions/GetTopUsers/lambda_function.py
@@ -10,18 +10,32 @@ from resultFormatter import getResultSingle, getResultMultiple, getResultError
 dynamodb_resource = resource('dynamodb')
 
 def lambda_handler(event, context):
-    data = get_items("User_Likes_Rank_Analysis")
-    users=data['Items']
-    users.sort(key=convert, reverse=False)
-    if len(users) >= event['limit']:
-        users=users[:event['limit']]
+    data = get_items("User_Likes_Rank_Analysis",None,None)
+    ranks=data['Items']
+    ranks.sort(key=convert, reverse=False)
+    if len(ranks) >= event['limit']:
+        ranks=ranks[:event['limit']]
+    users=getUsersByRanks(ranks)
     response=getResultMultiple(users)
     return response
 
-def get_items(table_name):
-    table = dynamodb_resource.Table(table_name)
-    data = table.scan()
-    return data
+def getUsersByRanks(ranks):
+    users = []
+    for rank in ranks:
+        filtering_exp = Attr('UserId').eq(rank['UserId'])
+        projection_exp='UserId, Email, UserName'
+        data = get_items("User",filtering_exp,projection_exp)
+        if data['Count'] > 0:
+            users.extend(data['Items'])
+    return users
 
 def convert(user):
     return user['Rank']
+
+def get_items(table_name, filterExpr, projExpr):
+    table = dynamodb_resource.Table(table_name)
+    if filterExpr==None and projExpr==None:
+        data = table.scan()
+    else:
+        data = table.scan(FilterExpression=filterExpr,ProjectionExpression=projExpr)
+    return data
