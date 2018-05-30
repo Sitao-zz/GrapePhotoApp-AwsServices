733a5830a96232fdc37c3c046063b8bfda582aea
Sitao Liang <liangsitao.ach@gmail.com>
2018-05-30 21:13:29 +0800

    1. Return the "Liked" status for the /post/my

--- LambdaFunctions/GetMyPosts/lambda_function.py
+++ LambdaFunctions/GetMyPosts/lambda_function.py
@@ -10,22 +10,35 @@ from resultFormatter import getResultSingle, getResultMultiple, getResultError
 dynamodb_resource = resource('dynamodb')
 
 def lambda_handler(event, context):
-    filtering_exp = Attr('UserId').eq(event['userid'])
-    projection_exp='PostId, #ts, ImgUrl, LikeCount, Note, UserId, ThumbUrl, Width, Height'
-    attrname_exp = { '#ts': 'Timestamp' }
-    data = get_items("Post",filtering_exp,projection_exp,attrname_exp)
-    posts = data['Items']
-    if len(posts) >= event['limit']:
-        posts=posts[:event['limit']]
-        posts.sort(key=convert, reverse=True)
-
+    posts = getPostsByUserId(event['userid'], event['limit'])
+    posts = updatePostLikesAttr(posts, event['userid'])
     response=getResultMultiple(posts)
     return response
 
 def convert(post):
     return post['Timestamp']
 
-def get_items(table_name, filterExpr, projExpr, attrname_exp):
+def getPostsByUserId(userid, limit):
+    filtering_exp = Attr('UserId').eq(userid)
+    data = get_items("Post",filtering_exp)
+    posts = data['Items']
+    if len(posts) >= limit:
+        posts=posts[:limit]
+        posts.sort(key=convert, reverse=True)
+    return posts
+
+def updatePostLikesAttr(posts, userid):
+    for post in posts:
+        filtering_exp = Attr('UserId').eq(userid)
+        filtering_exp &= Attr('PostId').eq(post['PostId'])
+        data = get_items('UserLike',filtering_exp)
+        if data['Count']>0:
+            post['Liked']=1
+        else:
+            post['Liked']=0
+    return posts
+
+def get_items(table_name, filterExpr):
     table = dynamodb_resource.Table(table_name)
-    data = table.scan(FilterExpression=filterExpr,ProjectionExpression=projExpr,ExpressionAttributeNames=attrname_exp)
+    data = table.scan(FilterExpression=filterExpr)
     return data
